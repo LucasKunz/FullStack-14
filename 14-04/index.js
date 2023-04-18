6382d1c720499cda3dc741aee55e2e2e3e9edd30
Lucas Kunz <lucaskunz1@gmail.com>
2023-04-17 22:02:19 -0300

    Co-authored-by: RafaelApolinario <RafaelApolinario@users.noreply.github.com> Co-authored-by: rafaellimagregorio <rafaellimagregorio@users.noreply.github.com> Co-authored-by: Guilherme Melo Bariviera <guilhermebari@users.noreply.github.com> Co-authored-by: Quym Machado <quymmachadodev@users.noreply.github.com> Co-authored-by: Mateus Silva <MateusSilva42@users.noreply.github.com> Co-authored-by: Fabricio Vianna <fabriciovnn@users.noreply.github.com> Co-authored-by: Lucas Paulus <lucaspaulus@users.noreply.github.com> Co-authored-by: anawesthpal <anawesthpal@users.noreply.github.com>

--- 14-04/index.js
+++ 14-04/index.js
@@ -7,51 +7,61 @@
 // # Ser maior que 1;
 // # Ser divisível apenas por 1 e por ele mesmo.
 
-for (let i = 30; i >= 1; i--) {
-  let isPrimeNumber = true
+let quantityPrimeNumbers = 0
+
+for (let i = 100; i >= 1; i--) {
+  let isPrimeNumber = false
 
   if (i <= 1) {
-    document.write(i)
+    console.log(i)
     continue
   }
 
   for (let n = 1; n < i; n++) {
     if (n === 1) continue
 
-    if (i % n == 0) {
+    if (i % n === 0) {
       isPrimeNumber = false
       break
     }
+
+    isPrimeNumber = true
+  }
+  if (i === 2) {
+    isPrimeNumber = true
   }
 
   if (isPrimeNumber) {
-    document.write(`[${i}]<br />`)
+    quantityPrimeNumbers++
+    console.log(`[${i}]`)
   } else {
-    document.write(`${i} <br />`)
+    console.log(`${i}`)
   }
 }
+
+console.log(quantityPrimeNumbers)
 
 
 // Aula Pratica 06 - Questão 9. Informe um valor a uma variável e imprima no console se o número é primo.
 
-const number = Number(prompt('Digite um número'))
+// const number = Number(prompt('Digite um número'))
 
-if (number <= 1) {
-  document.write(`O número ${number} não é um número primo`)
-} else {
-  let isPrimeNumber = true
-  for (let i = 1; i < number; i++) {
-    if (i === 1) continue
-    if (number % i === 0) {
-      isPrimeNumber = false
-      break
-    }
-  }
+// if (number <= 1) {
+//   document.write(`O número ${number} não é um número primo`)
+// } else {
+//   let isPrimeNumber = true
+//   for (let i = 1; i < number; i++) {
+//     if (i === 1) continue
+//     if (number % i === 0) {
+//       isPrimeNumber = false
+//       break
+//     }
+//   }
 
-  if (isPrimeNumber) {
-    document.write(`O número ${number} é um número primo`)
-  } else {
-    document.write(`O número ${number} não é um número primo`)
-  }
-}
+//   if (isPrimeNumber) {
+//     document.write(`O número ${number} é um número primo`)
+//   } else {
+//     document.write(`O número ${number} não é um número primo`)
+//   }
+// }
 
---
+++ 17-04/index.js
@@ -0,0 +1,167 @@
+// 1. Desenvolva um algoritmo para percorrer um vetor de 15 elementos inteiros já preenchido e
+// imprima todos os valores pares.
+
+
+// let elementos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+// for (const elemento of elementos) {
+//   if (elemento % 2 === 0) {
+//     document.write(`${elemento}<br>`)
+//   }
+// }
+
+// 2. Desenvolva um algoritmo para percorrer um vetor de 20 elementos inteiros e no final mostre a soma de todos os elementos.
+
+// let elementos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+// let soma = 0
+
+// for (let i = 0; i < elementos.length; i++) {
+//   soma += elementos[i]
+// }
+
+// document.write(soma);
+
+// 3. Desenvolva um algoritmo que preenche um vetor com os 4 primeiros números perfeitos.
+
+// const perfectNumber = []
+
+// for (let i = 1; perfectNumber.length < 2; i++) {
+//   let soma = 0
+
+//   for (let n = 1; n < i; n++) {
+//     if (i % n === 0) {
+//       soma += n
+//     }
+//   }
+
+//   if (i === soma) {
+//     perfectNumber.push(i)
+//   }
+// }
+// document.write(perfectNumber)
+
+// const perfectNumber = []
+// let number = 2
+// let divisorSum = 0
+
+// while (perfectNumber.length < 4) {
+//   for (let i = 1; i < number; i++) {
+//     if (number % i === 0) {
+//       divisorSum + i
+//     }
+//   }
+
+//   if (divisorSum === number) {
+//     perfectNumber.push(number)
+//   }
+
+//   divisorSum = 0
+//   number++
+// }
+
+// document.write(perfectNumber)
+
+
+// 4. Crie uma função que recebe 2 vetores de 10 elementos inteiros e que retorne a união dos dois em um novo vetor.
+
+
+// const vetor1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// const vetor2 = [11, 12, 13, 14, 15, 16, 17, 18, 19]
+// const vetoresUnidos = [];
+
+// for (const vetor of vetor1) {
+//   vetoresUnidos.push(vetor);
+// }
+
+// for (const vetor of vetor2) {
+//   vetoresUnidos.push(vetor);
+// }
+
+// document.write(`${vetoresUnidos}`)
+
+
+// function uniaoVetores(vetor1, vetor2) {
+//   vetoresUnidos = [];
+//   for (const vetor of vetor1) {
+//     vetoresUnidos.push(vetor);
+//   }
+//   for (const vetor of vetor2) {
+//     vetoresUnidos.push(vetor);
+//   }
+
+//   return vetoresUnidos
+// }
+
+// const uniao = uniaoVetores(vetor1, vetor2)
+
+// const vetor1 = []
+// const vetor2 = []
+// const vetorX = []
+// for (let x = 0; x < 3; x++) {
+//   vetor1.push(Number(prompt('digite um valor inteiro')))
+// }
+
+// for (let x = 0; x < 3; x++) {
+//   vetor2.push(Number(prompt('digite um valor inteiro')))
+// }
+
+// for (let elemento of vetor1) {
+//   vetorX.push(elemento)
+// }
+
+// for (let elemento of vetor2) {
+//   vetorX.push(elemento)
+// }
+
+// console.log(vetorX)
+
+// 5. Crie uma função que recebe um vetor de inteiros e um número inteiro e retorne verdadeiro se o número faz parte do vetor ou falso se não faz parte.
+
+// const array = []
+
+// for (let x = 0; x < 3; x++) {
+//   array.push(Number(prompt('digite um valor inteiro')))
+// }
+
+// let number = Number(prompt('digite um valor inteiro para verificar se existe no array'))
+// let msg = false
+
+// for (const num of array) {
+//   if (num === number) {
+//     msg = true
+//   }
+// }
+
+// for (let i = 0; i < array.length; i++) {
+//   if (array[i] === number) {
+//     msg = true
+//   }
+// }
+
+// console.log(msg)
+
+// 6. Escreva um algoritmo que solicite ao usuário a entrada de 5 nomes, e que exiba a lista desses nomes na tela. Após exibir essa lista, o programa deve mostrar também os nomes na ordem inversa em que o usuário os digitou.
+
+// Ex:
+// Entrada: Édson, Marcelo, Letícia, Ju, Thobias
+// Saída: Thobias, Ju, Letícia, Marcelo, Édson
+
+// const nomes = [];
+// const nomesReverse = []
+
+// for (let i = 1; i <= 5; i++) {
+//   const nome = prompt(`Digite o nome ${i}:`);
+//   nomes.push(nome);
+// }
+
+// for (const nome of nomes) {
+//   nomesReverse.unshift(nome)
+// }
+
+// console.log("Nomes digitados:");
+// console.log(nomes);
+
+// console.log("Nomes em ordem inversa:");
+// console.log(nomesReverse);
+
+// const msg = true
